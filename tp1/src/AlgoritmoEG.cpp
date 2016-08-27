a17ff79174fe4387420a1a35cc742276dcc87f56
panda <morbous.panda@gmail.com>
2016-08-26 21:28:16 -0300

    cambiamos todo

--- tp1/src/AlgoritmoEG.cpp
+++ tp1/src/AlgoritmoEG.cpp
@@ -22,15 +22,16 @@ Matriz& AlgoritmoEG::matriz(){
 void AlgoritmoEG::resolver(Vector &b,Vector &x) {
 	//Ax==b
 	int n = this->n;
+	Matriz matrizr = this->interna;
 	for(int i = 0; i < n-1; i++){
 		for(int j = i+1; j< n; j++){
-			double mji = this->interna[j][i]/this->interna[i][i];
+			double mji = matrizr[j][i]/matrizr[i][i];
 			//Ej = Ej - mji Ei
 			//LU[j] = LU[j] - mji*LU[i]
 			for(int k=i+1; k < n; k++){
-				cout << "i,j,k " << i << ", " << j << ", " << k << endl;
-				this->interna[j][k]= this->interna[j][k] - mji* this->interna[i][k];
-				this->interna[j][i]=mji;
+				//cout << "i,j,k " << i << ", " << j << ", " << k << endl;
+				matrizr[j][k]= matrizr[j][k] - mji* matrizr[i][k];
+				matrizr[j][i]=mji;
 			}
 			b[j] = b[j] - mji*b[i];
 		}
@@ -38,9 +39,9 @@ void AlgoritmoEG::resolver(Vector &b,Vector &x) {
 	
 	for(int i = n-1; i >= 0; i--){
 		for(int j = i+1; j< n; j++){
-			b[i] = b[i] - x[j]*this->interna[i][j];
+			b[i] = b[i] - x[j]*matrizr[i][j];
 		}
-		x[i] = b[i]/this->interna[i][i];
+		x[i] = b[i]/matrizr[i][i];
 	}
 }
 
--- tp1/src/main.cpp
+++ tp1/src/main.cpp
@@ -6,6 +6,7 @@
 #include "AlgoritmoLU.h"
 #include "funcionesDeMatriz.h"
 #include "AlgoritmoEG.h"
+#include <ctime>
 
 using namespace std;
 
@@ -15,7 +16,7 @@ using namespace std;
 //Funciones
 void cargarDatos(string pathEntrada);
 void calcularMatriz();
-void escribirFilaMatriz(int j, int tamMatriz,int radioActual);
+void escribirFilaMatriz(int j, int tamMatriz, int radioActual);
 void escribirSalida(string pathSalida, Vector vector);
 double valor_J_MenosUno_K(int j);
 double valor_J_K(int j);
@@ -40,12 +41,12 @@ double deltaAngulo;
 double deltaAnguloCuadrado;
 double deltaRadio;
 double deltaRadioCuadrado;
-Matriz matriz; 
+Matriz matriz;
 
 //Constante
 double pi = 3.1415926535897;
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 	if (argc < 4) {
 		cout << "Debe llamar al programa con <archivoDeEntrada> <archivoDeSalida> <algoritmo>" << endl;
 		return EXIT_SUCCESS;
@@ -53,15 +54,17 @@ int main(int argc, char *argv[]){
 	string archivoDeEntrada = argv[1];
 	string archivoDeSalida = argv[2];
 	string algoritmo = argv[3];
-	
+
 	cargarDatos(archivoDeEntrada);
-	
+
 	//Armar matriz de entrada
 	calcularMatriz();
 	AlgoritmoEG algEG;
 	AlgoritmoLU algLU;
-	
-	
+
+	algEG.inicializar(matriz, matriz.size());
+	algLU.inicializar(matriz, matriz.size());
+
 	/*LA RECONCHA DE TU MADRE C++ QUE NO TE DEJA HACER ESTAS COSAS*/
 	/*AlgoritmoInterface alg;
 	if(algoritmo == 0){
@@ -71,43 +74,33 @@ int main(int argc, char *argv[]){
 	}else{
 		cout << "Error en el parametro algoritmo" << endl; 
 	}*/
-	
-	for(int instancia = 0; instancia<cantDeInstancias;instancia++){
+	imprimir(matriz);
+	for (int instancia = 0; instancia < cantDeInstancias; instancia++) {
 		//Leer de archivDeEntrada las temperaturas para los radios internos y externos de la iesima instancia (linea i)
-		Vector b(tamMatriz,0);
-		Vector x(tamMatriz,0);
-		//alg.resolver(b,x);
-		if(algoritmo == "0"){
-			algEG.resolver(b,x);
-		}else if(algoritmo == "1"){
-			algLU.resolver(b,x);
-		}else{
-			cout << "Error en el parametro algoritmo" << endl; 
+		Vector b(matriz.size(), 0);
+		for (int i = 0; i < nAngulos; i++) {
+			b[i] = temperaturasInternas[instancia][i];
+			b[b.size() - nAngulos + i] = temperaturasExternas[instancia][i];
 		}
+		Vector x(matriz.size(), 0);
+		//alg.resolver(b,x);
+		if (algoritmo == "0") {
+			algEG.resolver(b, x);
+		} else if (algoritmo == "1") {
+			algLU.resolver(b, x);
+		} else {
+			cout << "Error en el parametro algoritmo" << endl;
+		}
+
+		//imprimir(x);
+		escribirSalida(archivoDeSalida, x);
 	}
-		
-	
-	    //matriz = Matriz(2, 2);
-	matriz = Matriz(2, Vector(2,0));
-	
-	matriz[0][0] = 1;
-	matriz[0][1] = 2;
-	matriz[1][0] = 3;
-	matriz[1][1] = 4;
-	
-    cargarDatos("D:/SVN FACU/Metnum2016_2c/tp1/src/tests/testAle.in");
-    calcularMatriz();
-    mostrarDatosCargados();
 
-    escribirSalida("D:/SVN FACU/Metnum2016_2c/tp1/src/tests/test3Ale.out",temperaturasInternas);
-    
 
-    
-    system("PAUSE");
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
-void cargarDatos(string pathEntrada){
+void cargarDatos(string pathEntrada) {
 	ifstream archivoDeEntrada;
 	archivoDeEntrada.open(pathEntrada.c_str());
 	archivoDeEntrada >> radioInterno;
@@ -117,134 +110,138 @@ void cargarDatos(string pathEntrada){
 	archivoDeEntrada >> isoterma;
 	archivoDeEntrada >> cantDeInstancias;
 
-	deltaRadio = (double)(radioExterno - radioInterno) / (double)(mMasUnoRadios-1);
-	deltaAngulo = convertirDeGradosARadianes((double)(360/nAngulos));
+	deltaRadio = (double) (radioExterno - radioInterno) / (double) (mMasUnoRadios - 1);
+	deltaAngulo = convertirDeGradosARadianes((double) (360 / nAngulos));
 	deltaRadioCuadrado = deltaRadio * deltaRadio;
 	deltaAnguloCuadrado = deltaAngulo * deltaAngulo;
 
-	temperaturasInternas = Matriz(cantDeInstancias,Vector(nAngulos,0));
-	temperaturasExternas = Matriz(cantDeInstancias,Vector(nAngulos,0));
+	temperaturasInternas = Matriz(cantDeInstancias, Vector(nAngulos, 0));
+	temperaturasExternas = Matriz(cantDeInstancias, Vector(nAngulos, 0));
 
 	double temp;
-	for(int j = 0 ; j< cantDeInstancias;j++){
-		for(int i = 0; i < nAngulos; i++){
-			archivoDeEntrada >> temp;       
+	for (int j = 0; j < cantDeInstancias; j++) {
+		for (int i = 0; i < nAngulos; i++) {
+			archivoDeEntrada >> temp;
 			temperaturasInternas[j][i] = temp;
 		}
-		for(int i = 0; i < nAngulos; i++){
-			archivoDeEntrada >> temp;       
+		for (int i = 0; i < nAngulos; i++) {
+			archivoDeEntrada >> temp;
 			temperaturasExternas[j][i] = temp;
 		}
 	}
 	archivoDeEntrada.close();
-} 
+}
 
-void calcularMatriz(){
+void calcularMatriz() {
 	int tamMatriz = nAngulos * mMasUnoRadios;
 	int radioActual = 0;
-	matriz = Matriz(tamMatriz, Vector(tamMatriz,0));
-	for(int i = 0; i < tamMatriz; i++){
-		escribirFilaMatriz(i, tamMatriz,radioActual);
-		if(i > (radioActual)* (mMasUnoRadios-1)){
+	matriz = Matriz(tamMatriz, Vector(tamMatriz, 0));
+	for (int i = 0; i < tamMatriz; i++) {
+		escribirFilaMatriz(i, tamMatriz, radioActual);
+		if (i > (radioActual)* (mMasUnoRadios - 1)) {
 			radioActual++;
 		}
-	}     
+	}
 }
 
-void escribirFilaMatriz(int j, int tamMatriz, int radioActual){
-     if(j < nAngulos || j > (tamMatriz-nAngulos-1)){
-          matriz[j][j] = 1;
-     }else{
-          int posInicialDelRadio = (radioActual-1) * (mMasUnoRadios-1);
-          int posFinalDelRadio = (posInicialDelRadio-1) + (mMasUnoRadios-1);
-          matriz[j][j] = valor_J_K(radioActual);
-          matriz[j][j-mMasUnoRadios+1] = valor_J_MasUno_K(radioActual);//-1;                                      
-          matriz[j][j+mMasUnoRadios-1] = valor_J_MenosUno_K(radioActual);//1;                   
-          double valorCaundoK_EsMasMenosUno =  valor_J_K_MasMenosUno(radioActual);
-          if(j==posInicialDelRadio){
-               matriz[j][posFinalDelRadio] = valorCaundoK_EsMasMenosUno;
-          }else{
-                matriz[j][j-1] = valorCaundoK_EsMasMenosUno;
-          }
-          if(j==posFinalDelRadio){
-               matriz[j][posInicialDelRadio] = valorCaundoK_EsMasMenosUno;
-          }else{
-                matriz[j][j+1] = valorCaundoK_EsMasMenosUno;
-          }        
-          
-     }
+void escribirFilaMatriz(int j, int tamMatriz, int radioActual) {
+	if (j < nAngulos || j > (tamMatriz - nAngulos - 1)) {
+		matriz[j][j] = 1;
+	} else {
+		int posInicialDelRadio = (radioActual) * (mMasUnoRadios - 1);
+		int posFinalDelRadio = (posInicialDelRadio - 1) + (mMasUnoRadios - 1);
+		matriz[j][j] = valor_J_K(radioActual);
+		matriz[j][j - mMasUnoRadios + 1] = valor_J_MasUno_K(radioActual); //-1;                                      
+		matriz[j][j + mMasUnoRadios - 1] = valor_J_MenosUno_K(radioActual); //1;                   
+		double valorCaundoK_EsMasMenosUno = valor_J_K_MasMenosUno(radioActual);
+		if (j == posInicialDelRadio) {
+			matriz[j][posFinalDelRadio] = valorCaundoK_EsMasMenosUno;
+		} else {
+			matriz[j][j - 1] = valorCaundoK_EsMasMenosUno;
+		}
+		if (j == posFinalDelRadio) {
+			matriz[j][posInicialDelRadio] = valorCaundoK_EsMasMenosUno;
+		} else {
+			matriz[j][j + 1] = valorCaundoK_EsMasMenosUno;
+		}
 
-     
+	}
+
+
 }
 
+void escribirSalida(string pathSalida, Vector vector) {
 
-void escribirSalida(string pathSalida, Vector vector){
-     
-     ofstream archivoDeSalida;
-     archivoDeSalida.open(pathSalida.c_str());
-     for(int j = 0; j < vector.size(); j++){
-             archivoDeSalida <<  vector[j] << endl;
-     }
-     archivoDeSalida.close();
+	FILE * archivoDeSalida = fopen(pathSalida.c_str(), "w");
+	string dato;
+	//archivoDeSalida.open(pathSalida.c_str());
+	for (int j = 0; j < vector.size(); j++) {
+//		 sprintf(dato.c_str(),"%f.6", vector[j]);
+//		 archivoDeSalida << dato << endl;
+		fprintf(archivoDeSalida, "%.06f\n", vector[j]);
+	}
+	fclose(archivoDeSalida);
 }
 
-double valor_J_MenosUno_K(int j){
-      double radio = radioInterno + (deltaRadio * j);
-      return (double)((1/deltaRadioCuadrado)-(1/(deltaRadio*radio)));
+double valor_J_MenosUno_K(int j) {
+	double radio = radioInterno + (deltaRadio * j);
+	return (double) ((1 / deltaRadioCuadrado)-(1 / (deltaRadio * radio)));
 }
 
-double valor_J_K(int j){
-      double radio = radioInterno + (deltaRadio * j);
-      double radioCuadarado = radio * radio;
-      return (double)((-2/deltaRadioCuadrado)+(1/(deltaRadio*radio))-(2/(deltaAnguloCuadrado*radioCuadarado)));
+double valor_J_K(int j) {
+	double radio = radioInterno + (deltaRadio * j);
+	double radioCuadarado = radio * radio;
+	return (double) ((-2 / deltaRadioCuadrado)+(1 / (deltaRadio * radio))-(2 / (deltaAnguloCuadrado * radioCuadarado)));
 }
 
-double valor_J_MasUno_K(int j){
-      return (double)(1/deltaRadioCuadrado);
+double valor_J_MasUno_K(int j) {
+	return (double) (1 / deltaRadioCuadrado);
 }
 
-double valor_J_K_MasMenosUno(int j){
-      double radio = radioInterno + (deltaRadio * j);
-      double radioCuadarado = radio * radio;
-      
-      return (double)(1/(deltaAnguloCuadrado*radioCuadarado));
+double valor_J_K_MasMenosUno(int j) {
+	double radio = radioInterno + (deltaRadio * j);
+	double radioCuadarado = radio * radio;
+
+	return (double) (1 / (deltaAnguloCuadrado * radioCuadarado));
 }
 
-double convertirDeGradosARadianes(double grados){
-       return (grados * 2 /360) * pi;
+double convertirDeGradosARadianes(double grados) {
+	return (grados * 2 / 360) * pi;
 }
 
-void mostrarDatosCargados(){
+void mostrarDatosCargados() {
 
-    cout << "radioInterno: " << radioInterno << endl;
-    cout << "radioExterno: " << radioExterno << endl;
-    cout << "mMasUnoRadios: " << mMasUnoRadios << endl;
-    cout << "nAngulos: " << nAngulos << endl;
-    cout << "isoterma: " << isoterma << endl;
-    cout << "cantDeInstancias: " << cantDeInstancias << endl;
-    cout << "deltaRadio: " << deltaRadio << endl;
-    cout << "deltaAngulo: " << deltaAngulo << endl;
-    
-    cout << "temperaturasInternas: ";
-    mostrarVector(temperaturasInternas);
-    cout << "temperaturasExternas: ";
-    mostrarVector(temperaturasExternas);
-    
-    cout << "Matiz: " << endl; 
-    mostrarMatriz(matriz, matriz.size());
+	cout << "radioInterno: " << radioInterno << endl;
+	cout << "radioExterno: " << radioExterno << endl;
+	cout << "mMasUnoRadios: " << mMasUnoRadios << endl;
+	cout << "nAngulos: " << nAngulos << endl;
+	cout << "isoterma: " << isoterma << endl;
+	cout << "cantDeInstancias: " << cantDeInstancias << endl;
+	cout << "deltaRadio: " << deltaRadio << endl;
+	cout << "deltaAngulo: " << deltaAngulo << endl;
+
+	cout << "temperaturasInternas: ";
+	imprimir(temperaturasInternas);
+	cout << "temperaturasExternas: ";
+	imprimir(temperaturasExternas);
+
+	cout << "Matiz: " << endl;
+	mostrarMatriz(matriz, matriz.size());
 }
 
-void mostrarVector(Vector vactor){
-     for(int j = 0; j < vactor.size(); j++){
-             cout << vactor[j] << " ";
-     } cout << endl;
+void mostrarVector(Vector vactor) {
+	for (int j = 0; j < vactor.size(); j++) {
+		cout << vactor[j] << " ";
+	}
+	cout << endl;
 }
 
-
-void mostrarMatriz(Matriz matriz, int tam){
-     for(int i = 0; i < tam; i++){
-             for(int j = 0; j < tam; j++){
-                     cout << matriz[i][j] << " ";
-             } cout << endl;
-     }cout << endl;
+void mostrarMatriz(Matriz matriz, int tam) {
+	for (int i = 0; i < tam; i++) {
+		for (int j = 0; j < tam; j++) {
+			cout << matriz[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
